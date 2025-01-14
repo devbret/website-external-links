aa807d716ddae7dbd67e6ec8e18d02e8e47f95f3
Bret Bernhoft <transhumansorcery@protonmail.com>
2025-01-13 22:38:29 -0800

    Add detailed logging with a CSV file.

--- README.md
+++ README.md
@@ -8,8 +8,8 @@ Use Python to map a website's external facing links. And then apply D3 to visual
 
 ### Programs Needed
 
--   [Git](https://git-scm.com/downloads)
--   [Python](https://www.python.org/downloads/) (When installing on Windows, make sure you check the ["Add python 3.xx to PATH"](https://hosting.photobucket.com/images/i/bernhoftbret/python.png) box.)
+- [Git](https://git-scm.com/downloads)
+- [Python](https://www.python.org/downloads/) (When installing on Windows, make sure you check the ["Add python 3.xx to PATH"](https://hosting.photobucket.com/images/i/bernhoftbret/python.png) box.)
 
 ### Steps
 
@@ -18,6 +18,6 @@ Use Python to map a website's external facing links. And then apply D3 to visual
 3. Clone this repository using `git` by running the following command; `git clone https://github.com/devbret/website-external-links`.
 4. Navigate to the repo's directory by running; `cd website-external-links`.
 5. Install the needed dependencies for running the script by running; `pip install -r requirements.txt`.
-6. Edit the app.py file on line 41, to include the website that you would like to visualize. You can also change the maximum number of URLs that this program will visit at a given domain, by editing the "max_links" value on line 9 in the app.py file; which is set to 100 by default.
+6. Edit the app.py file on line 51, to include the website that you would like to visualize. You can also change the maximum number of URLs that this program will visit at a given domain, by editing the "max_links" value on line 11 in the app.py file; which is set to 50 by default.
 7. Run the script with the command `python3 app.py`.
 8. To view the website's connections in the index.html file you will need to run a local web server. To do this run `python3 -m http.server`.
--- app.py
+++ app.py
@@ -2,36 +2,46 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin, urlparse
 import json
+import csv
+from datetime import datetime
 
 def is_external(url, base):
     return urlparse(url).netloc != urlparse(base).netloc
 
-def crawl_site(start_url, max_links=100):
+def crawl_site(start_url, max_links=50, csv_filename='discovered_links.csv'):
     visited = set()
     external_links = {}
 
-    def crawl(url):
-        if len(visited) >= max_links:
-            return
-        visited.add(url)
-        print(f"Crawling: {url}")
-        try:
-            response = requests.get(url, timeout=5)
-            soup = BeautifulSoup(response.text, 'html.parser')
-        except requests.exceptions.RequestException as e:
-            print(f"Failed to crawl {url}: {e}")
-            return
+    with open(csv_filename, mode='w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(['URL', 'Type', 'Timestamp'])
 
-        for link in soup.find_all('a', href=True):
-            href = urljoin(url, link.get('href'))
-            if is_external(href, start_url):
-                if href not in external_links:
-                    external_links[href] = []
-                external_links[href].append(url)
-            elif href not in visited:
-                crawl(href)
+        def crawl(url):
+            if len(visited) >= max_links:
+                return
+            visited.add(url)
+            timestamp = datetime.now().isoformat()
+            csv_writer.writerow([url, 'internal', timestamp])
+            print(f"Crawling: {url}")
+            try:
+                response = requests.get(url, timeout=5)
+                soup = BeautifulSoup(response.text, 'html.parser')
+            except requests.exceptions.RequestException as e:
+                print(f"Failed to crawl {url}: {e}")
+                return
 
-    crawl(start_url)
+            for link in soup.find_all('a', href=True):
+                href = urljoin(url, link.get('href'))
+                if is_external(href, start_url):
+                    if href not in external_links:
+                        external_links[href] = []
+                    external_links[href].append(url)
+                    csv_writer.writerow([href, 'external', timestamp])
+                elif href not in visited:
+                    crawl(href)
+
+        crawl(start_url)
+
     return external_links
 
 def save_links_as_json(external_links, filename='external_links.json'):
